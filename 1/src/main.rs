afddf2113308c25a7d975e5dbb51249278e812fc
lukedds <luke.3.smith@gmail.com>
2019-11-27 15:07:11 +0000

    Update main.rs

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,21 +1,12 @@
-use std::vec::Vec;
-
 fn main() {
-
-    let mut values = Vec::new();
-
-    for x in 0..1000 {
-        if x%5 == 0 || x%3 == 0 {
-            values.push(x);
-        }
-    }
 
     let mut answer:u32 = 0;
 
-    for y in values {
-        answer = answer + y;
+    for x in 0..1000 {
+        if x%5 == 0 || x%3 == 0 {
+            answer = answer + x;
+        }
     }
 
     println!("{}", answer);
-
 }
